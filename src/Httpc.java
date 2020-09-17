c02e62213de14cf90dc7da6f324c2af72264631f
bowen01 <bowen.yang@mail.concordia.ca>
2020-09-18 04:28:54 +0800

    1.add while loop to program 2.finish dealing with " " in -h and -d 3.finsh deal with " ' in -h -d 4.leave help part for tomorrow

--- src/Httpc.java
+++ src/Httpc.java
@@ -4,47 +4,80 @@ import java.util.HashMap;
 public class Httpc {
     public static void main(String[] args) {
 
-        // Read the command from the keyboard
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the Httpc Command: ");
-        String input = sc.nextLine();
-        String[] inputArr = input.split(" ");
-
-        //Request property
-        String method = "GET";  // GET is the default Request Type
-        boolean isShowHeader = false;   // Not show the header is the default setting
-        HashMap<String, String> headers = new HashMap<>();
-        String body = "";
-        String filePath = "";
-        String url = "";
-
-        for (int i = 0; i < inputArr.length; i++) {
-            if (inputArr[i].toLowerCase().equals("post")) {
-                method = "POST";
-            } else if (inputArr[i].equals("-v")) {
-                isShowHeader = true;
-            } else if (inputArr[i].equals("-h")) {
-                //looking for next item, and put it in the Hashmap
-                i++;
-                String[] header = inputArr[i].substring(1, inputArr[i].length() - 1).split(":");
-                String key = header[0];
-                String value = header[1];
-                headers.put(key, value);
-            } else if (inputArr[i].equals("-d")) {
-                body = inputArr[++i];
-            } else if (inputArr[i].equals("-f")) {
-                filePath = inputArr[++i];
-            } else if (inputArr[i].matches("^http://(.)*")) {
-                url = inputArr[i];
+        while (true){
+            // Read the command from the keyboard
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Please enter the Httpc Command(-q for exit): ");
+            String input = sc.nextLine();
+            if (input.equals("-q")){
+                break;
             }
+            //deal with the {"Assignment":      1}
+            input=input.replaceAll(":\\s+",":");
+            System.out.println(input);
+
+            String[] inputArr = input.split(" ");
+            //Request property
+            String method = "GET";  // GET is the default Request Type
+            boolean isShowHeader = false;   // Not show the header is the default setting
+            HashMap<String, String> headers = new HashMap<>();
+            String body = "";
+            String filePath = "";
+            String url = "";
+
+            for (int i = 0; i < inputArr.length; i++) {
+                if (inputArr[i].toLowerCase().equals("post")) {
+                    method = "POST";
+                } else if (inputArr[i].equals("-v")) {
+                    isShowHeader = true;
+                } else if (inputArr[i].equals("-h")) {
+                    //looking for next item, and put it in the Hashmap
+                    i++;
+                    String[] header = inputArr[i].split(":");
+                    if (header.length<2){
+                        System.out.println("Header's format is invalid");
+                    }else {
+                        String key = header[0];
+                        String value = header[1];
+                        // deal with " "
+                        if (key.startsWith("\"")||key.startsWith("\'")){
+                            key=key.substring(1,key.length());
+                        }
+                        if (value.endsWith("\"")||value.endsWith("\'")){
+                            value=value.substring(0,value.length()-1);
+                        }
+                        headers.put(key, value);
+                    }
+
+                } else if (inputArr[i].equals("-d")) {
+                    body = inputArr[++i];
+                    // deal with " "
+                    if (body.startsWith("\"")||body.startsWith("\'")){
+                        body=body.substring(1,body.length());
+                    }
+                    if (body.endsWith("\"")||body.endsWith("\'")){
+                        body=body.substring(0,body.length()-1);
+                    }
+                } else if (inputArr[i].equals("-f")) {
+                    filePath = inputArr[++i];
+                } else if (inputArr[i].matches("^http://(.)*")) {
+                    url = inputArr[i];
+                }
+                else if(inputArr[i].equals("help")){
+                    System.out.println("httpc is a curl-like application but supports HTTP protocol only.");
+
+                }
+            }
+
+            System.out.println("requestMethod: " + method);
+            System.out.println("isShowHeader: " + isShowHeader);
+            System.out.println("headers: " + headers);
+            System.out.println("body: " + body);
+            System.out.println("filePath: " + filePath);
+            System.out.println("url: " + url);
         }
 
-        System.out.println("requestMethod: " + method);
-        System.out.println("isShowHeader: " + isShowHeader);
-        System.out.println("headers: " + headers);
-        System.out.println("body: " + body);
-        System.out.println("filePath: " + filePath);
-        System.out.println("url: " + url);
+
     }
 
 }
