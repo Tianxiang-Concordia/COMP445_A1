3d560c4ed3fbd797a485c8deb718417f2459178f
bowen01 <bowen.yang@mail.concordia.ca>
2020-09-18 22:55:52 +0800

    1.改好了

--- src/Httpc.java
+++ src/Httpc.java
@@ -17,68 +17,69 @@ public class Httpc {
             //help part
             if (input.matches("^httpc help.*")) {
                 printHelp(input);
-            } else {
-                //deal with the {"Assignment":      1}
-                input = input.replaceAll(":\\s+", ":");
-                System.out.println(input);
+                continue;
+            }
+            //deal with the {"Assignment":      1}
+            input = input.replaceAll(":\\s+", ":");
+            System.out.println(input);
 
-                String[] inputArr = input.split(" ");
-                //Request property
-                String method = "GET";  // GET is the default Request Type
-                boolean isShowHeader = false;   // Not show the header is the default setting
-                HashMap<String, String> headers = new HashMap<>();
-                String body = "";
-                String filePath = "";
-                String url = "";
+            String[] inputArr = input.split(" ");
+            //Request property
+            String method = "GET";  // GET is the default Request Type
+            boolean isShowHeader = false;   // Not show the header is the default setting
+            HashMap<String, String> headers = new HashMap<>();
+            String body = "";
+            String filePath = "";
+            String url = "";
 
-                for (int i = 0; i < inputArr.length; i++) {
-                    if (inputArr[i].toLowerCase().equals("post")) {
-                        method = "POST";
-                    } else if (inputArr[i].equals("-v")) {
-                        isShowHeader = true;
-                    } else if (inputArr[i].equals("-h")) {
-                        //looking for next item, and put it in the Hashmap
-                        i++;
-                        String[] header = inputArr[i].split(":");
-                        if (header.length < 2) {
-                            System.out.println("Header's format is invalid");
-                        } else {
-                            String key = header[0];
-                            String value = header[1];
-                            // deal with " "
-                            if (key.startsWith("\"") || key.startsWith("\'")) {
-                                key = key.substring(1, key.length());
-                            }
-                            if (value.endsWith("\"") || value.endsWith("\'")) {
-                                value = value.substring(0, value.length() - 1);
-                            }
-                            headers.put(key, value);
-                        }
-
-                    } else if (inputArr[i].equals("-d")) {
-                        body = inputArr[++i];
+            for (int i = 0; i < inputArr.length; i++) {
+                if (inputArr[i].toLowerCase().equals("post")) {
+                    method = "POST";
+                } else if (inputArr[i].equals("-v")) {
+                    isShowHeader = true;
+                } else if (inputArr[i].equals("-h")) {
+                    //looking for next item, and put it in the Hashmap
+                    i++;
+                    String[] header = inputArr[i].split(":");
+                    if (header.length < 2) {
+                        System.out.println("Header's format is invalid");
+                    } else {
+                        String key = header[0];
+                        String value = header[1];
                         // deal with " "
-                        if (body.startsWith("\"") || body.startsWith("\'")) {
-                            body = body.substring(1, body.length());
+                        if (key.startsWith("\"") || key.startsWith("\'")) {
+                            key = key.substring(1, key.length());
                         }
-                        if (body.endsWith("\"") || body.endsWith("\'")) {
-                            body = body.substring(0, body.length() - 1);
+                        if (value.endsWith("\"") || value.endsWith("\'")) {
+                            value = value.substring(0, value.length() - 1);
                         }
-                    } else if (inputArr[i].equals("-f")) {
-                        filePath = inputArr[++i];
-                    } else if (inputArr[i].matches("^http://(.)*")) {
-                        url = inputArr[i];
+                        headers.put(key, value);
                     }
 
+                } else if (inputArr[i].equals("-d")) {
+                    body = inputArr[++i];
+                    // deal with " "
+                    if (body.startsWith("\"") || body.startsWith("\'")) {
+                        body = body.substring(1, body.length());
+                    }
+                    if (body.endsWith("\"") || body.endsWith("\'")) {
+                        body = body.substring(0, body.length() - 1);
+                    }
+                } else if (inputArr[i].equals("-f")) {
+                    filePath = inputArr[++i];
+                } else if (inputArr[i].matches("^http://(.)*")) {
+                    url = inputArr[i];
                 }
 
-                System.out.println("requestMethod: " + method);
-                System.out.println("isShowHeader: " + isShowHeader);
-                System.out.println("headers: " + headers);
-                System.out.println("body: " + body);
-                System.out.println("filePath: " + filePath);
-                System.out.println("url: " + url);
             }
+
+            System.out.println("requestMethod: " + method);
+            System.out.println("isShowHeader: " + isShowHeader);
+            System.out.println("headers: " + headers);
+            System.out.println("body: " + body);
+            System.out.println("filePath: " + filePath);
+            System.out.println("url: " + url);
+
 
         }
     }
