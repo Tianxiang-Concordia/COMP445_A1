d657649d2afcc5675b988c8221ad55a2423a0de0
Tianxiang Ying <896267775@qq.com>
2020-09-21 21:35:38 -0400

    Refactoring the whole project

--- src/test/util.java
+++
@@ -1,8 +0,0 @@
-package test;
-import static handlers.FileHandler.writeToFile;
-
-public class util {
-    public static void main(String[] args) {
-        writeToFile("test.txt",true,"test02");
-    }
-}
